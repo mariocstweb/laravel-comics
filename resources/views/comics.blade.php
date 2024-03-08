0460cb2c413ca10827bc39c3fc26cc579f723438
mariocstweb <mariocstmra@gmail.com>
2024-03-08 16:42:02 +0100

    added  comics-book section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.main')
 
 @section('main-content')
-
+<section id="comics-books">
+  <div class="comics-container">
+    <div class="info">
+      <h1>{{$movie['title']}}</h1>
+      <div class="price">
+        <div class="price-left">
+          <span>U.S. PRICE {{$movie['price']}}</span>
+          <span>AVAILABLE</span>
+        </div>
+        <div class="price-right">
+          <span>Check Availability</span>
+        </div>
+      </div>
+      <p>{{$movie['description']}}</p>
+    </div>
+    <div class="banner">
+      <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+    </div>
+  </div>
+</section>
 @endsection

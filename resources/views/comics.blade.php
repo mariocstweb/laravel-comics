3d5b28561f7be8e43b32126f80b9b914d133969a
mariocstweb <mariocstmra@gmail.com>
2024-03-08 17:23:01 +0100

    added comics-info section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -21,4 +21,65 @@
     </div>
   </div>
 </section>
+<section id="comics-info">
+  <div class="comics-container">
+    <div class="talent">
+      <h4>Talent</h4>
+      <div class="art">
+        <div class="text">
+          <p>Art by:</p>
+        </div>
+        <div class="list">
+          <ul>
+            @foreach($movie['artists'] as $movie['artist'])
+            <li>{{$movie['artist']}},</li>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+      <div class="art">
+        <div class="text">
+          <p>Written by:</p>
+        </div>
+        <div class="list">
+          <ul>
+            @foreach($movie['writers'] as $movie['writer'])
+            <li>{{$movie['writer']}},</li>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+    <div class="specs">
+      <h4>Specs</h4>
+      <div class="art">
+        <div class="text">
+          <p>Series:</p>
+        </div>
+        <div class="list">
+          <p>{{$movie['series']}}</p>
+        </div>
+      </div>
+      <div class="art">
+        <div class="text">
+          <p>U.S Price:</p>
+        </div>
+        <div class="list">
+          <p> {{$movie['price']}}</p>
+        </div>
+      </div>
+      <div class="art">
+        <div class="text">
+          <p>On Sale Date:</p>
+        </div>
+        <div class="list">
+          <p>{{$movie['sale_date']}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
